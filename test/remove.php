46607a84c3ed97ca8c644e48222a1d6e10daee56
Timothy Totten <2010@totten.ca>
2024-06-19 16:16:43 -0700

    fixed test/remove.php to avoid weird inconsistencies

--- test/remove.php
+++ test/remove.php
@@ -5,73 +5,89 @@ namespace Lum\Tests;
 require_once 'vendor/autoload.php';
 
 use Lum\Arrays as A;
+use Lum\Test as LT;
 
-$t = new \Lum\Test();
+$test = new LT();
+$test->plan(28);
 
-$t->plan(28);
-
-$arrays = 
-[
-  [1,2,2,5,7,2,1,2],
-  ["foo", "bar", "baz", "foo"],
-  [1,2,3,"1","2","3"],
-  ["hello"=>"world", "goodbye"=>"world", "foo"=>"bar"],
-];
-
-$msgs =
-[
-  'list'  => 'list modified as expected',
-  'count' => 'returned expected count', 
-];
-
-$strict = true;
-
-function rm($a, $v, $er, $ea)
+class Tester
 {
-  global $arrays, $msgs, $strict, $t;
-  $array = $arrays[$a];
-  $r = A::remove($array, $v, $strict);
-  $an = 'A'.$a+1;
-  $n = "remove($an, ".json_encode($v).", ".json_encode($strict).")";
-  $t->is($r, $er, "$n ${msgs['count']}");
-  $t->isJSON($array, $ea, "$n ${msgs['list']}");
+  public LT $test;
+
+  public $arrays = 
+  [
+    [1,2,2,5,7,2,1,2],
+    ["foo", "bar", "baz", "foo"],
+    [1,2,3,"1","2","3"],
+    ["hello"=>"world", "goodbye"=>"world", "foo"=>"bar"],
+  ];
+
+  public $msgs =
+  [
+    'list'  => 'list modified as expected',
+    'count' => 'returned expected count', 
+  ];
+
+  public bool $strict = true;
+
+  public function __construct(LT $test)
+  {
+    $this->test = $test;
+  }
+
+  public function rm($a, $v, $er, $ea)
+  {
+    $t = $this->test;
+    $msgs = $this->msgs;
+    $array = $this->arrays[$a];
+
+    $r = A::remove($array, $v, $this->strict);
+    $an = 'A'.$a+1;
+    $n = "remove($an, ".json_encode($v).", ".json_encode($this->strict).")";
+    $t->is($r, $er, "$n {$msgs['count']}");
+    $t->isJSON($array, $ea, "$n {$msgs['list']}");
+  }
+
+  public function rma($a, $vs, $er, $ea)
+  {
+    $t = $this->test;
+    $msgs = $this->msgs;
+    $array = $this->arrays[$a];
+
+    A::$remove_all_strict = $this->strict;
+    $r = A::remove_all($array, ...$vs);
+    $an = 'A'.$a+1;
+    $n = ($this->strict?'':'strict=false; ')
+      ."remove_all($an, ...".json_encode($vs).")";
+    $t->is($r, $er, "$n {$msgs['count']}");
+    $t->isJSON($array, $ea, "$n {$msgs['list']}");
+  }
 }
 
-function rma($a, $vs, $er, $ea)
-{
-  global $arrays, $msgs, $strict, $t;
-  $array = $arrays[$a];
-  A::$remove_all_strict = $strict;
-  $r = A::remove_all($array, ...$vs);
-  $an = 'A'.$a+1;
-  $n = ($strict?'':'strict=false; ')
-    ."remove_all($an, ...".json_encode($vs).")";
-  $t->is($r, $er, "$n ${msgs['count']}");
-  $t->isJSON($array, $ea, "$n ${msgs['list']}");
-}
+$t = new Tester($test);
 
-rm(0, 2, 4, [1,5,7,1]);
-rm(0, "2", 0, $arrays[0]);
-$strict = false;
-rm(0, "2", 4, [1,5,7,1]);
+$t->rm(0, 2, 4, [1,5,7,1]);
+$t->rm(0, "2", 0, $t->arrays[0]);
+$t->strict = false;
+$t->rm(0, "2", 4, [1,5,7,1]);
 
-$strict = true;
-rm(1, "foo", 2, ["bar","baz"]);
-rm(2, 2, 1, [1,3,"1","2","3"]);
-$strict = false;
-rm(2, 2, 2, [1,3,"1","3"]);
-$strict = true;
-rm(3, "world", 2, ["foo"=>"bar"]);
+$t->strict = true;
+$t->rm(1, "foo", 2, ["bar","baz"]);
+$t->rm(2, 2, 1, [1,3,"1","2","3"]);
+$t->strict = false;
+$t->rm(2, 2, 2, [1,3,"1","3"]);
+$t->strict = true;
+$t->rm(3, "world", 2, ["foo"=>"bar"]);
 
-rma(0, [1,2], 6, [5,7]);
-rma(1, ["bar","baz"], 2, ["foo","foo"]);
-rma(2, [1,3], 2, [2,"1","2","3"]);
-$strict = false;
-rma(2, [1,3], 4, [2,"2"]);
-$strict = true;
-rma(3, ["world","bar"], 3, []);
-rma(3, ["invalid"], 0, $arrays[3]);
-rma(3, [], 0, $arrays[3]);
+$t->rma(0, [1,2], 6, [5,7]);
+$t->rma(1, ["bar","baz"], 2, ["foo","foo"]);
+$t->rma(2, [1,3], 2, [2,"1","2","3"]);
+$t->strict = false;
+$t->rma(2, [1,3], 4, [2,"2"]);
+$t->strict = true;
+$t->rma(3, ["world","bar"], 3, []);
+$t->rma(3, ["invalid"], 0, $t->arrays[3]);
+$t->rma(3, [], 0, $t->arrays[3]);
 
-echo $t->tap();
-return $t;
+echo $test->tap();
+return $test;
